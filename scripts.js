e745896a5d56f4385af2596bc833bac153e236c2
Vlad Spreys <vlad@spreys.com>
2019-03-31 18:01:38 +1300

    Login functionality finished

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
+const JWT_KEY = "jwt";
 
 function retrieveCompetitions(callaback) {
     $.get("/api/competition/read.php", function (data) {
         callaback(data)
     });
+}
+
+function login(username, password, callback) {
+    $.ajax({
+        url: '/api/users/login.php',
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            sessionStorage.setItem(JWT_KEY, data.jwt);
+            callback(true);
+        },
+        error: function(data) {
+            callback(false);
+        },
+        data: JSON.stringify({
+            email: username,
+            password: password
+        })
+    });
+}
+
+function isTokenPresent() {
+    return sessionStorage.getItem(JWT_KEY) !== null;
 }
